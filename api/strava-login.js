e6b2a21475531d6b69dc1eb175e79c8ae7625549
bonniesuen3268-oss <bonniesuen3268@gmail.com>
2026-03-09 16:00:55 +1100

    Update strava-login.js

--- api/strava-login.js
+++ api/strava-login.js
@@ -1,18 +1,11 @@
-exports.handler = async (event) => {
-  const clientId = process.env.STRAVA_CLIENT_ID;
-  const redirectUri = process.env.URL + "/api/strava-callback";
-
-  const stravaAuthUrl =
-    `https://www.strava.com/oauth/authorize` +
-    `?client_id=${clientId}` +
-    `&redirect_uri=${encodeURIComponent(redirectUri)}` +
-    `&response_type=code` +
-    `&approval_prompt=auto` +
-    `&scope=activity:read_all`;
-
-  return {
-    statusCode: 302,
-    headers: { Location: stravaAuthUrl },
-    body: "",
-  };
+module.exports = function(req, res) {
+  var clientId = process.env.STRAVA_CLIENT_ID;
+  var redirectUri = "https://bonnies-marathon-plan.vercel.app/api/strava-callback";
+  var url = "https://www.strava.com/oauth/authorize" +
+    "?client_id=" + clientId +
+    "&redirect_uri=" + encodeURIComponent(redirectUri) +
+    "&response_type=code" +
+    "&approval_prompt=auto" +
+    "&scope=activity:read_all";
+  res.redirect(url);
 };
